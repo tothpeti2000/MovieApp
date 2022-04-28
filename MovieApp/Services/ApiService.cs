7930ec252455ec21b1c1b435cc97fee659f1822f
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-04-28 19:31:38 +0200

    Comments added to GetAsync in ApiService

--- MovieApp/Services/ApiService.cs
+++ MovieApp/Services/ApiService.cs
@@ -10,15 +10,21 @@ namespace MovieApp.Services
 {
     public class ApiService
     {
+        // API URL and apiKey which is required for all requests
         private readonly Uri baseURL = new Uri("https://api.themoviedb.org/3");
         private readonly string apiKey = "780aae170739c0d86c29396d5f6d279f";
 
+        // Generic GET request
         public async Task<T> GetAsync<T>(string path)
         {
             using (var client = new HttpClient())
             {
+                // Add the apiKey to each request as a query param
+                path += $"&api_key={apiKey}";
+                // Create the full request URL by combining the baseURL and the request specific path
                 var uri = new Uri(baseURL, path);
 
+                // Convert the response JSON to an object with type T
                 var response = await client.GetAsync(uri);
                 var json = await response.Content.ReadAsStringAsync();
 
