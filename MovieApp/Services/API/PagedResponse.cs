1fa657b3670d79a99194cff092b59644dc8b52b4
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-04-28 20:43:13 +0200

    Generic PagedResponse added for requests using paging

---
+++ MovieApp/Services/API/PagedResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp.Services.API
+{
+    public class PagedResponse<T>
+    {
+        public int Page { get; set; }
+        public T[] Results { get; set; }
+        public int Total_pages { get; set; }
+        public int Total_results { get; set; }
+    }
+}
