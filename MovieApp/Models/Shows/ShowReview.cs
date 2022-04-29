9cc4b2a626faa0d666199dec43b9b4b905d11fc7
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-04-29 09:21:28 +0200

    ShowReview model class added

---
+++ MovieApp/Models/Shows/ShowReview.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp.Models.Shows
+{
+    public class ShowReview
+    {
+        public string ID { get; set; }
+        public string Author { get; set; }
+        public Author Author_Details { get; set; }
+        public string Content { get; set; }
+        public DateTime Created_At { get; set; }
+        public DateTime Updated_At { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class Author
+    {
+        public string name { get; set; }
+        public string username { get; set; }
+        public string avatar_path { get; set; }
+        public int rating { get; set; }
+    }
+}
