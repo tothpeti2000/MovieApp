411f4ea6917cf13278b6533d2d3aafaf0229cf4f
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-04-29 15:09:16 +0200

    SimilarMovie model class added

---
+++ MovieApp/Models/Movies/SimilarMovie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp.Models.Movies
+{
+    public class SimilarMovie
+    {
+        public string poster_path { get; set; }
+        public bool adult { get; set; }
+        public string overview { get; set; }
+        public string release_date { get; set; }
+        public int[] genre_ids { get; set; }
+        public int id { get; set; }
+        public string original_title { get; set; }
+        public string original_language { get; set; }
+        public string title { get; set; }
+        public object backdrop_path { get; set; }
+        public float popularity { get; set; }
+        public int vote_count { get; set; }
+        public bool video { get; set; }
+        public float vote_average { get; set; }
+    }
+}
