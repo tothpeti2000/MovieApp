b76e7946888ab9f2cfce954c041870225f0593f4
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-04-29 15:13:58 +0200

    Discover Movie requests added

--- MovieApp/Services/Movies/IMovieService.cs
+++ MovieApp/Services/Movies/IMovieService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MovieApp.Models.Movies;
+using MovieApp.Services.API;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,7 @@ namespace MovieApp.Services.Movies
 {
     public interface IMovieService
     {
-
+        Task<PagedResponse<SimilarMovie>> GetMoviesWithGenreIDAsync(int genreID);
+        Task<PagedResponse<SimilarMovie>> GetMoviesWithKeywordIDsAsync(int[] keywordIDs);
     }
 }
--- MovieApp/Services/Movies/MovieService.cs
+++ MovieApp/Services/Movies/MovieService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MovieApp.Models.Movies;
+using MovieApp.Services.API;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,30 @@ using System.Threading.Tasks;
 
 namespace MovieApp.Services.Movies
 {
-    public class MovieService
+    public class MovieService : IMovieService
     {
+        private readonly IApiService apiService;
 
+        public MovieService(IApiService apiService)
+        {
+            this.apiService = apiService;
+        }
+
+        public async Task<PagedResponse<SimilarMovie>> GetMoviesWithGenreIDAsync(int genreID)
+        {
+            return await apiService.GetAsync<PagedResponse<SimilarMovie>>($"discover/movie?with_genres={genreID}");
+        }
+
+        public async Task<PagedResponse<SimilarMovie>> GetMoviesWithKeywordIDsAsync(int[] keywordIDs)
+        {
+            string totalKeywordIDs = "";
+
+            foreach (var ID in keywordIDs)
+            {
+                totalKeywordIDs += $"{ID},";
+            }
+
+            return await apiService.GetAsync<PagedResponse<SimilarMovie>>($"discover/movie?with_keywords={totalKeywordIDs}");
+        }
     }
 }
