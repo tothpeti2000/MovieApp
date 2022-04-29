96970a2012b6a095142f0867dac87af00d4ce3cc
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-04-29 09:29:39 +0200

    SimilarShow model class added

---
+++ MovieApp/Models/Shows/SimilarShow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp.Models.Shows
+{
+    public class SimilarShow
+    {
+        public int ID { get; set; }
+        public string Backdrop_Path { get; set; }
+        public string First_Air_Date { get; set; }
+        public int[] Genre_Ids { get; set; }
+        public string Original_Language { get; set; }
+        public string Original_Name { get; set; }
+        public string Overview { get; set; }
+        public string[] Origin_Country { get; set; }
+        public string Poster_Path { get; set; }
+        public float Popularity { get; set; }
+        public string Name { get; set; }
+        public float Vote_Average { get; set; }
+        public int Vote_Count { get; set; }
+    }
+}
