09adf464e94fc83fecc739c451485bd0b6be332f
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-05-01 21:46:24 +0200

    ShortEpisodeDetails and ExtendedEpisodeDetails extracted

--- MovieApp/Models/Shows/ExtendedShowDetails.cs
+++ MovieApp/Models/Shows/ExtendedShowDetails.cs
@@ -88,32 +88,6 @@ namespace MovieApp.Models.Shows
         public int VoteCount { get; set; }
     }
 
-    public class ShortEpisodeDetails
-    {
-        public int ID { get; set; }
-
-        [JsonProperty("air_date")]
-        public string AirDate { get; set; }
-
-        [JsonProperty("episode_number")]
-        public int EpisodeNumber { get; set; }
-
-        public string Name { get; set; }
-        public string Overview { get; set; }
-
-        [JsonProperty("season_number")]
-        public int SeasonNumber { get; set; }
-
-        [JsonProperty("still_path")]
-        public string StillPath { get; set; }
-
-        [JsonProperty("vote_average")]
-        public float VoteAverage { get; set; }
-
-        [JsonProperty("vote_count")]
-        public int VoteCount { get; set; }
-    }
-
     public class Creator
     {
         public int ID { get; set; }
--- MovieApp/Models/Shows/ShortEpisodeDetails.cs
+++ MovieApp/Models/Shows/ShortEpisodeDetails.cs
@@ -7,22 +7,29 @@ using System.Threading.Tasks;
 
 namespace MovieApp.Models.Shows
 {
-    public class ExtendedSeasonDetails
+    public class ShortEpisodeDetails
     {
         public int ID { get; set; }
 
         [JsonProperty("air_date")]
         public string AirDate { get; set; }
 
+        [JsonProperty("episode_number")]
+        public int EpisodeNumber { get; set; }
+
         public string Name { get; set; }
         public string Overview { get; set; }
-
-        [JsonProperty("poster_path")]
-        public string PosterPath { get; set; }
 
         [JsonProperty("season_number")]
         public int SeasonNumber { get; set; }
 
-        public ExtendedEpisodeDetails[] Episodes { get; set; }
+        [JsonProperty("still_path")]
+        public string StillPath { get; set; }
+
+        [JsonProperty("vote_average")]
+        public float VoteAverage { get; set; }
+
+        [JsonProperty("vote_count")]
+        public int VoteCount { get; set; }
     }
 }
