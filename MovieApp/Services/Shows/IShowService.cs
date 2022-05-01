4440ea598384613462953b04e518497ad36447ba
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-05-01 18:54:52 +0200

    Initial methods added to IShowService

--- MovieApp/Services/Shows/IShowService.cs
+++ MovieApp/Services/Shows/IShowService.cs
@@ -10,10 +10,16 @@ namespace MovieApp.Services.Shows
 {
     public interface IShowService
     {
-        Task<PagedResponse<PopularShow>> GetPopularShowsAsync();
-        Task<ShowDetails> GetShowDetailsByIDAsync(int ID);
-        Task<Credits> GetShowCreditsByIDAsync(int ID);
-        Task<PagedResponse<ShowReview>> GetShowReviewsByIDAsync(int ID);
-        Task<PagedResponse<SimilarShow>> GetSimilarShowsByIDAsync(int ID);
+        Task<PagedResponse<PopularShow>> GetPopularAsync();
+        Task<ShowDetails> GetDetailsByIDAsync(int ID);
+        Task<PagedResponse<>> GetByGenreAsync();
+        Task<PagedResponse<>> GetByKeywordsAsync();
+        Task<PagedResponse<>> GetByQueryAsync();
+        Task<Credits> GetCreditsByIDAsync(int ID);
+        Task<PagedResponse<>> GetSimilarAsync();
+        Task<> GetSeasonDetailsByIDAsync(int ID, int season);
+        Task<> GetEpisodeDetailsByIDAsync(int ID, int season, int episode);
+
+        Task<> GetGenresAsync();
     }
 }
