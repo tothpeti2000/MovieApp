e6ab9169016a0c93658029d3d0f8ad55be478f69
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-05-02 13:01:09 +0200

    GetByGenreAsync and GetByKeywordsAsync added

--- MovieApp/Services/Movies/IMovieService.cs
+++ MovieApp/Services/Movies/IMovieService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MovieApp.Models.Movies;
+using MovieApp.Services.API;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,7 @@ namespace MovieApp.Services.Movies
 {
     public interface IMovieService
     {
+        Task<PagedResponse<ShortMovieDetails>> GetByGenreAsync(int genreID, int page = 1);
+        Task<PagedResponse<ShortMovieDetails>> GetByKeywordsAsync(int[] keywordIDs, int page = 1);
     }
 }
