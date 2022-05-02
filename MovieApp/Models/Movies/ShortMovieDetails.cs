65ab7cab09b65a850b360a1dea41be49bc0d7483
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-05-02 13:01:25 +0200

    ShortMovieDetails model class added

---
+++ MovieApp/Models/Movies/ShortMovieDetails.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp.Models.Movies
+{
+    public class ShortMovieDetails
+    {
+        [JsonProperty("poster_path")]
+        public string PosterPath { get; set; }
+
+        public bool Adult { get; set; }
+        public string Overview { get; set; }
+
+        [JsonProperty("release_date")]
+        public string ReleaseDate { get; set; }
+
+        [JsonProperty("genre_ids")]
+        public int[] GenreIDs { get; set; }
+
+        [JsonProperty("original_title")]
+        public string OriginalTitle { get; set; }
+
+        [JsonProperty("original_language")]
+        public string OriginalLanguage { get; set; }
+
+        public string Title { get; set; }
+
+        [JsonProperty("backdrop_path")]
+        public string BackdropPath { get; set; }
+
+        public float Popularity { get; set; }
+
+        [JsonProperty("vote_count")]
+        public int VoteCount { get; set; }
+
+        public bool Video { get; set; }
+
+        [JsonProperty("vote_average")]
+        public float VoteAverage { get; set; }
+    }
+}
